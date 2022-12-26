cda2d77b6b64f4b2f3490c396364a16cf35936c7
Ndingossoka <107273136+epipilass@users.noreply.github.com>
2022-12-26 12:19:18 +0100

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,8 @@
+const txtAnim = document.querySelector('h2');
+new Typewriter(textAnim, {
+
+})
+.typeString('PARCOURS ACADEMIQUE')
+.pauseFor(300)
+.typeString('<Strong>,Dev Full-Stack</Strong>')
+.start()
